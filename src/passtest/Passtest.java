67f424240ad29acbc9fdaa4dddb9503f669efbd4
zeusz3 <>
2015-07-04 19:31:08 +0300

    CHANGES: -clean up after the new allSymbols -> allowedSymbols mapping redesign TODO: -update the app with the new code

--- src/passtest/Passtest.java
+++ src/passtest/Passtest.java
@@ -8,10 +8,8 @@ package passtest;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Hashtable;
-import java.util.Map.Entry;
 import java.util.TreeSet;
 import static java.lang.Math.floorMod;
-import static java.lang.Math.ceil;
 
 /**
  *
@@ -36,23 +34,17 @@ public class Passtest {
         try {
             md = MessageDigest.getInstance("SHA-512");
             byte[][] hash = new byte[5][];
-            hash[0] = md.digest("p10".getBytes());
-            hash[1] = md.digest("dist".getBytes());
-            hash[2] = md.digest("2014".getBytes());
-            hash[3] = md.digest("epso".getBytes());
-            hash[4] = md.digest("t".getBytes());
+            hash[0] = md.digest("test".getBytes());
+            hash[1] = md.digest("test".getBytes());
+            hash[2] = md.digest("test".getBytes());
+            hash[3] = md.digest("test".getBytes());
+            hash[4] = md.digest("test".getBytes());
             StringBuilder sb = new StringBuilder(hash[0].length);
-            //for(int i = 0; i < hash[0].length && i < outputlen*(hash[0].length/outputlen); i+=(hash[0].length/(outputlen))) {
             for(int i = 0; i < hash[0].length; i++) {
                 int b = 0;
-                for(int j = 0; j < 2; j++) {
-                    //System.out.println(hash[j][i]);
-              //  for (int j = 0; j < hash.length; j++) {
+                for(int j = 0; j < hash.length; j++) {
                     b ^= hash[j][i];
                 }
-                //sb.append(allowedSymbols.get((byte)b));
-                //System.out.println(b);
-                //System.out.println(floorMod(b, allowedSymbols.size()));
                 sb.append(allowedSymbols.get(floorMod(b, allowedSymbols.size())));
             }
             System.out.println((int)64/12);
@@ -61,57 +53,45 @@ public class Passtest {
             System.out.println(sb.toString());
             System.out.println(sb.length());
             System.out.println(sb.substring(0,outputlen));
-            for(int i = 0, j=0; i < 64 && j < outputlen; i+=64/outputlen, j++) {
+            for(int i = 0, j=0; i < hash[0].length && j < outputlen; i+=hash[0].length/outputlen, j++) {
                 System.out.print(sb.charAt(i));
             }
-            //editText2.setText(Integer.toHexString(hash[0]));
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
-//        for(Entry<Byte, String> entry : dict.entrySet()) {
-//            System.out.print(entry.getKey() + " - " +  entry.getValue());
-//        }
     }
     
     private static void initializeDict() {
     	bannedSymbols.add('\"');
     	bannedSymbols.add('\\');
     	bannedSymbols.add('\'');
-//        bannedSymbols.add("`");
-//        bannedSymbols.add("~");
-//        bannedSymbols.add("#");
-//        bannedSymbols.add("%");
-//        bannedSymbols.add("^");
-//        bannedSymbols.add("(");
-//        bannedSymbols.add(")");
-//        bannedSymbols.add("-");
-//        //bannedSymbols.add("+");
-//        bannedSymbols.add("+");
-//        bannedSymbols.add("{");
-//        bannedSymbols.add("[");
-//        bannedSymbols.add("}");
-//        bannedSymbols.add("]");
-//        bannedSymbols.add("|");
-//        bannedSymbols.add(";");
-//        bannedSymbols.add(":");
-//        bannedSymbols.add("/");
-//        bannedSymbols.add(".");
-//        bannedSymbols.add(">");
-//        bannedSymbols.add(",");
-//        bannedSymbols.add("<");
-//        bannedSymbols.add("=");
+        bannedSymbols.add('`');
+        bannedSymbols.add('~');
+        bannedSymbols.add('#');
+        bannedSymbols.add('%');
+        bannedSymbols.add('^');
+        bannedSymbols.add('(');
+        bannedSymbols.add(')');
+        bannedSymbols.add('-');
+        bannedSymbols.add('+');
+        bannedSymbols.add('{');
+        bannedSymbols.add('[');
+        bannedSymbols.add('}');
+        bannedSymbols.add(']');
+        bannedSymbols.add('|');
+        bannedSymbols.add(';');
+        bannedSymbols.add(':');
+        bannedSymbols.add('/');
+        bannedSymbols.add('.');
+        bannedSymbols.add('>');
+        bannedSymbols.add(',');
+        bannedSymbols.add('<');
+        bannedSymbols.add('=');
         int j = 0;
     	for(int i = 0; i < allSymbols.length; i++) {
             if(!bannedSymbols.contains(allSymbols[i])) {
                 allowedSymbols.put(j++, allSymbols[i]);
             }
-//            } else {
-//                while(bannedSymbols.contains(String.valueOf(allSymbols[++j % allSymbols.length]))) {
-//                        //j++;
-//                }
-//                allowedSymbols.put(String.valueOf(allSymbols[j % allSymbols.length]), (byte)i);
-//            }
-//            j++;
     	}
     }
 }
